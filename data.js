d7ada47f62dc9f870f178ef6ca9179f18c8597f6
Deepali Singh <104728592+DeepaliSingh1995@users.noreply.github.com>
2025-01-06 14:52:59 +0000

    static data api

---
+++ data.js
@@ -0,0 +1,9 @@
+
+const data =[ 
+    {name : 'Deepali', email : 'deepali@gmail.com'},
+    {name : 'Anubhav', email : 'a@gmail.com'},
+    {name : 'Testing', email : 't@gmail.com'},
+    {name : 'New user', email : 'n@gmail.com'},
+    {name : 'User', email : 'u@gmail.com'}
+]
+module.exports = data;
